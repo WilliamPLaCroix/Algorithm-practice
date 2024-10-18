232f4ff1b3d134bbedbb4abcec25a6196e0b3711
WilliamPLaCroix <williamplacroix@gmail.com>
2024-10-18 17:24:27 +0200

    define LR test class

--- test_sample.py
+++ test_sample.py
@@ -1,3 +1,5 @@
+import pytest
+
 import sklearn as sk
 from sklearn.linear_model import LinearRegression as sklr
 import numpy as np
@@ -8,23 +10,25 @@ import linear_model as lm
 import random
 random.seed(42)
 
-
-def test_linear_regression_fit():
-    test_X = np.array([random.randint(-100, 100) for i in range(10)]).reshape(-1, 1)
-    test_y = np.array([random.randint(-10, 10) for i in range(10)])
-    model = lm.LinearRegression()
-    model.fit(test_X, test_y)
+class TestLinearRegression:
+    X = np.array([random.randint(-100, 100) for i in range(10)]).reshape(-1, 1)
+    y = np.array([random.randint(-10, 10) for i in range(10)])
+    test_model = lm.LinearRegression()
+    test_model.fit(X, y)
     verification_model = sk.linear_model.LinearRegression()
-    verification_model.fit(test_X, test_y)
-    assert format(model.intercept, '.15f') == format(verification_model.intercept_, '.15f')
-    assert format(model.coef, '.16f') == format(verification_model.coef_[0], '.16f')
+    verification_model.fit(X, y)
 
-# def test_linear_regression_coef():
-#     test_X = np.array([random.randint(-100, 100) for i in range(10)]).reshape(-1, 1)
-#     test_y = np.array([random.randint(-10, 10) for i in range(10)])
-#     model = lm.LinearRegression()
-#     model.fit(test_X, test_y)
-#     verification_model = sk.linear_model.LinearRegression()
-#     verification_model.fit(test_X, test_y)
-#     assert format(model.coef, '.16f') == format(verification_model.coef_[0], '.16f')
+    def test_coef(self):
+        assert format(self.test_model.coef, '.16f') == format(self.verification_model.coef_[0], '.16f')
+    
+    def test_intercept(self):
+        assert format(self.test_model.intercept, '.15f') == format(self.verification_model.intercept_, '.15f')
+
+    def test_predict(self):
+        with pytest.raises(NotImplementedError):
+            self.test_model.predict(self.X)
+
+    def test_score(self):
+        assert self.test_model.score == None
+        #assert format(self.test_model.score(self.X, self.y), '.16f') == format(self.verification_model.score(self.X, self.y), '.16f')
 
