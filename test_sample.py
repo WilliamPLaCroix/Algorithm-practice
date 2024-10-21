617f027ff580b40bdbb5cc97cd2aec922134ca4c
WilliamPLaCroix <williamplacroix@gmail.com>
2024-10-21 15:14:25 +0200

    add tests for predict /  score

--- test_sample.py
+++ test_sample.py
@@ -8,7 +8,6 @@ import linear_model as lm
 
 
 import random
-random.seed(42)
 
 class TestLinearRegression:
     X = np.array([random.randint(-100, 100) for i in range(10)]).reshape(-1, 1)
@@ -19,16 +18,21 @@ class TestLinearRegression:
     verification_model.fit(X, y)
 
     def test_coef(self):
-        assert format(self.test_model.coef, '.16f') == format(self.verification_model.coef_[0], '.16f')
+        assert format(self.test_model.coef_.item(), '.14f') == format(self.verification_model.coef_[0], '.14f')
     
     def test_intercept(self):
-        assert format(self.test_model.intercept, '.15f') == format(self.verification_model.intercept_, '.15f')
+        assert format(self.test_model.intercept_, '.14f') == format(self.verification_model.intercept_, '.14f')
 
     def test_predict(self):
-        with pytest.raises(NotImplementedError):
-            self.test_model.predict(self.X)
+        passed = True
+        for pair in zip(self.test_model.predict(self.X), self.verification_model.predict(self.X)):
+            if format(pair[0], '14f') != format(pair[1], '14f'):
+                passed = False
+        assert passed == True
 
     def test_score(self):
-        assert self.test_model.score == None
-        #assert format(self.test_model.score(self.X, self.y), '.16f') == format(self.verification_model.score(self.X, self.y), '.16f')
+        assert format(self.test_model.score(self.X, self.y), '.14f') == format(self.verification_model.score(self.X, self.y), '.14f')
 
+    def test_score_weights(self):
+        with pytest.raises(NotImplementedError):
+            self.test_model.score(self.X, self.y, np.ones((len(self.y), 1)))
